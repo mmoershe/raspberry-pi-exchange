23bdae208ac4e69c4698c9cc0f6e616b5b3c6a87
mmoershe <92526921+mmoershe@users.noreply.github.com>
2024-03-19 19:30:18 +0100

    moved files and added venv to gitignore

--- legacy_rasp_4/testing.py
+++ legacy_rasp_4/testing.py
@@ -1,55 +1,35 @@
-# This uses the 7-Segment-Display
-import RPi.GPIO as GPIO
-import time
+print("hitler")
+num = []
 
-# GPIO.setwarnings(True)
-
-NUM = [0xffc0,0xfff9,0xffa4,0xffb0,0xff99,0xff92,0xff82,0xfff8,0xff80,0xff90]
-LSBFIRST = 1
-MSBFIRST = 2
-# define the pins for 74HC595
-dataPin   = 15      # DS Pin of 74HC595(Pin14)
-latchPin  = 13      # ST_CP Pin of 74HC595(Pin12)
-clockPin = 11       # CH_CP Pin of 74HC595(Pin11)
-
-def destroy():
-    GPIO.cleanup()
-    print("--- Script will be closed. Bye! ---")
-def setup():
-    GPIO.setmode(GPIO.BOARD)   # use PHYSICAL GPIO Numbering
-    GPIO.setup(dataPin, GPIO.OUT)
-    GPIO.setup(latchPin, GPIO.OUT)
-    GPIO.setup(clockPin, GPIO.OUT)
-    GPIO.output(dataPin, GPIO.LOW)   
-    GPIO.output(latchPin, GPIO.LOW)
-    GPIO.output(clockPin, GPIO.LOW)
-    
-def onoff():
-    GPIO.output(clockPin, GPIO.HIGH)
-    GPIO.output(clockPin, GPIO.LOW)
-
-if __name__ == "__main__":
-    print("--- Script is starting. ---")
-    setup()
-    
-    GPIO.output(dataPin, GPIO.HIGH)
-    onoff()
-    onoff()
-    onoff()
-    onoff()
-    GPIO.output(dataPin, GPIO.LOW)
-    onoff()
-    onoff()
-    onoff()
-    GPIO.output(dataPin, GPIO.HIGH)
-    onoff()
-    onoff()
-    onoff()
-    onoff()
-    onoff()
-    onoff()
-    onoff()    
-    
-    GPIO.output(latchPin, GPIO.HIGH)
-    time.sleep(2)
-    destroy()
+thisdict = {
+  0: 0xffc0,
+  1: 0xfff9,
+  2: 0xffa8,
+  3: 0xffb0,
+  4: 0xff99,
+  5: 0xff92,
+  6: 0xff82,
+  7: 0xfff8,
+  8: 0xff80,
+  9: 0xff90,
+  "a": 0xff88,
+  "b": 0xff83,
+  "c": 0xffc6,
+  "d": 0xffa1,
+  "e": 0xff86,
+  "f": 0xff8e,
+  "g": 0xffe2,
+  "h": 0xff8d,
+  "i": 0xffcf,
+  "j": 0xfff1,
+  "l": 0xffc7,
+  "n": 0xffc8,
+  "p": 0xff8c,
+  "q": 0xff98,
+  "r": 0xffaf,
+  "t": 0xff87,
+  "U": 0xffc1,
+  "y": 0xff91,
+}
+for i in range(0, len(thisdict)):
+    num.append(thisdict[i])
